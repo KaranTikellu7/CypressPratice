91a641cbd4a156fae0793923a28a0bd6b485d3a1
Karan Tikellu <tikellukaran@gmail.com>
2025-07-30 14:44:22 +0530

    5-Verify Advertisement card

--- cypress/support/pages/mainPage.js
+++ cypress/support/pages/mainPage.js
@@ -1,7 +1,25 @@
+import mainPage from '../locators/mainPage.json'
+
 class MainPage {
-    verifyNavLinks(){
-        cy.xpath('//div[@class="nav-links "]').should('be.exist')
-    }
+  verifyNavLinks() {
+    const navLinks = ['Products', 'Add Product', 'Practice', 'Learn', 'Login']
+    cy.xpath(mainPage.mainPageNavLinks).should('be.visible')
+
+    cy.xpath(mainPage.mainPageNavLinks).each(($elements, index) => {
+      cy.wrap($elements)
+        .should('have.text', navLinks[index])
+        .and('have.attr', 'href')
+    })
+  }
+
+  verifyAdvertisementCard() {
+    cy.xpath(mainPage.banner).should('be.visible')
+    cy.xpath(mainPage.bannerText).should(
+      'have.text',
+      'ADVERTISE YOUR PRODUCT / SERVICE HERE: Contact me on X @CommitQuality'
+    )
+    cy.xpath(mainPage.bannerAdvertismentLink).should('have.attr', 'href')
+  }
 }
 
 export default MainPage
